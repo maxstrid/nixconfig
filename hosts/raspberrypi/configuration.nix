4d0338bfd1279f255725ca03e98430c538e2ca22
Maxwell Henderson <mxwhenderson@gmail.com>
2024-01-27 11:49:08 -0800

    Update rpi bootloader
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/raspberrypi/configuration.nix
+++ hosts/raspberrypi/configuration.nix
@@ -6,10 +6,14 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-  boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
+  boot.loader = {
+    raspberryPi = {
+      enable = true;
+      version = 4;
+    };
+    grub.enable = true;
+    generic-extlinux-compatible.enable = true;
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
 

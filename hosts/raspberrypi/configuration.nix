0a6a78f3e64e94ca489fd1f948a26ced4fbbd95e
Maxwell Henderson <mxwhenderson@gmail.com>
2024-01-27 12:20:18 -0800

    Remove uneeded options in configuration.nix
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/raspberrypi/configuration.nix
+++ hosts/raspberrypi/configuration.nix
@@ -18,8 +18,6 @@
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
-    keyMap = "us";
-    useXkbConfig = true; # use xkb.options in tty.
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.

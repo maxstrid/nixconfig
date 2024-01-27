723ab3789f56ff58724cd58124ced24f38bdb994
Maxwell Henderson <mxwhenderson@gmail.com>
2024-01-27 13:12:45 -0800

    Update boot to include rpi4 kernel package
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/raspberrypi/configuration.nix
+++ hosts/raspberrypi/configuration.nix
@@ -7,6 +7,8 @@
     ];
 
   boot.loader = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
     grub.enable = false;
     generic-extlinux-compatible.enable = true;
   };

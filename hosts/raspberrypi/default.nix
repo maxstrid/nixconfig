c429a13bcfb60078bd988eaf82539b050f75120e
Maxwell Henderson <mxwhenderson@gmail.com>
2024-01-27 19:49:17 -0800

    Add services.nix with jellyfin setup
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/raspberrypi/default.nix
+++ hosts/raspberrypi/default.nix
@@ -3,5 +3,6 @@
 {
   imports = [
     ./configuration.nix
+    ./services.nix
   ];
 }
---
+++ hosts/raspberrypi/services.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.systemPackages = [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
+  ];
+
+  services = {
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+    };
+  };
+}

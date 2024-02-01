60577fff67b14af84ad3923b6446dad5728622c4
Maxwell Henderson <mxwhenderson@gmail.com>
2024-02-01 01:17:53 -0800

    Setup qbittorrent and gluetun
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/rpi4/services.nix
+++ hosts/rpi4/services.nix
@@ -33,6 +33,45 @@
             "3001:3001"
           ];
         };
+
+        gluetun = {
+          autoStart = true;
+          image = "qmcgaw/gluetun:v3.37.0";
+          ports = [
+            # Qbittorrent
+            "8080:8080"
+            "6881:6881"
+            "6881:6881/udp"
+          ];
+          environment = {
+            VPN_SERVICE_PROVIDER = "mullvad";
+            VPN_TYPE = "wireguard";
+            WIREGUARD_PRIVATE_KEY = config.sops.secrets.wireguard_private_key;
+            WIREGUARD_ADDRESSES = config.sops.secrets.wireguard_addresses;
+          };
+          extraOptions = [
+            "--cap-add=NET_ADMIN"
+          ];
+        };
+
+        qbittorrent = {
+          autoStart = true;
+          image = "lscr.io/linuxserver/qbittorrent:latest";
+          environment = {
+            PUID = 1000;
+            PGID = 1000;
+            TZ = "US/Pacific";
+            WEBUI_PORT = 8080;
+          };
+          volumes = [
+            "/home/docker/qbittorrent/config/:/config"
+            "/mnt/hdd/downloads:/downloads"
+          ];
+          dependsOn = ["gluetun"];
+          extraOptions = [
+            "--network=container:gluetun"
+          ];
+        };
       };
     };
   };

41cdcf3233ad525865e83f517a45b942ecb677e4
Maxwell Henderson <mxwhenderson@gmail.com>
2024-01-31 23:12:37 -0800

    Setup basic secrets with sops
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,18 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, nixos-hardware, ... }:
+    { nixpkgs, nixos-hardware, sops-nix, ... }:
     {
       nixosConfigurations = {
         rpi4 = nixpkgs.lib.nixosSystem {
           system = "aarch64-linux";
           modules = [
+            sops-nix.nixosModules.sops
             nixos-hardware.nixosModules.raspberry-pi-4 
+
             ./hosts/rpi4
           ];
         };
---
+++ hosts/rpi4/sops.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ... }:
+
+{
+  sops.defaultSopsFile = ./secrets/rpi4.yaml;
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  sops.secrets."gluetun/wireguard_private_key" = {};
+  sops.secrets."gluetun/wireguard_adresses" = {};
+}

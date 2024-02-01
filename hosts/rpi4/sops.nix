c9054758a2185dc7b34baaad82af329abf22636d
Maxwell Henderson <mxwhenderson@gmail.com>
2024-02-01 00:21:52 -0800

    Re-arrange secrets
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/rpi4/sops.nix
+++ hosts/rpi4/sops.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  sops.defaultSopsFile = ./secrets/rpi4.yaml;
+  sops.defaultSopsFile = ../../secrets.yaml;
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.secrets."gluetun/wireguard_private_key" = {};
   sops.secrets."gluetun/wireguard_adresses" = {};

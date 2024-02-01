1fadbfdbb0fc9e8c089f84dfbdc73e859b0f2a10
Maxwell Henderson <mxwhenderson@gmail.com>
2024-02-01 01:06:34 -0800

    Update secrets for better organization
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/rpi4/sops.nix
+++ hosts/rpi4/sops.nix
@@ -3,6 +3,8 @@
 {
   sops.defaultSopsFile = ./secrets.yaml;
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  sops.secrets.wireguard_private_key = {};
-  sops.secrets.wireguard_addresses = {};
+  sops.secrets = {
+      wireguard_private_key = {};
+      wireguard_addresses = {};
+  };
 }

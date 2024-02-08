c2dc40c8d857248516d9660d8c49cbcdef210d54
Maxwell Henderson <mxwhenderson@gmail.com>
2024-02-07 18:11:39 -0800

    m900: Enable bcachefs
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/m900/default.nix
+++ hosts/m900/default.nix
@@ -9,8 +9,13 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+      };
+      supportedFilesystems = [ "bcachefs" ];
+  };
 
   networking.hostName = "m900";
   time.timeZone = "US/Pacific";

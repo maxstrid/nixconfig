68a4f609819a7f5d8ad3e94453a68b7c2da122c6
Maxwell Henderson <mxwhenderson@gmail.com>
2024-02-13 14:44:13 -0800

    rpi4: Switch to using networking.wireguard
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/rpi4/vpn.nix
+++ hosts/rpi4/vpn.nix
@@ -1,41 +1,36 @@
 { config, pkgs, ... }:
 
 {
-  networking = {
-    firewall.allowedUDPPorts = [51820];
-    useNetworkd = true;
+  # enable NAT
+  networking.nat.enable = true;
+  networking.nat.externalInterface = "eth0";
+  networking.nat.internalInterfaces = [ "wg0" ];
+  networking.firewall = {
+    allowedUDPPorts = [ 51820 ];
   };
 
-  systemd.network = {
-    enable = true;
-    netdevs = {
-      "50-wg0" = {
-        netdevConfig = {
-          Kind = "wireguard";
-          Name = "wg0";
-          MTUBytes = "1300";
-        };
-        wireguardConfig = {
-          PrivateKeyFile = "${config.sops.secrets.wireguard-private-key.path}";
-          ListenPort = 51820;
-        };
-        wireguardPeers = [
-          {
-            wireguardPeerConfig = {
-              PublicKey = "K26Ii1PeoV/MiHnlR5AVliT1Gxcj6l9e7d2a/JhxPGE=";
-              AllowedIPs = ["10.100.0.2"];
-            };
-          }
-        ];
-      };
-    };
-    networks.wg0 = {
-      matchConfig.Name = "wg0";
-      address = ["10.100.0.1/24"];
-      networkConfig = {
-        IPMasquerade = "ipv4";
-        IPForward = true;
-      };
+  networking.wireguard.interfaces = {
+    wg0 = {
+      ips = [ "10.100.0.1/24" ];
+
+      listenPort = 51820;
+
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      privateKeyFile = "${config.sops.secrets.wireguard-private-key.path}";
+
+      peers = [
+        {
+          publicKey = "K26Ii1PeoV/MiHnlR5AVliT1Gxcj6l9e7d2a/JhxPGE=";
+          allowedIPs = [ "10.100.0.2/32" ];
+        }
+      ];
     };
   };
 }

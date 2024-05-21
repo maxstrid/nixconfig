f421c37b9016af8c3d402af671d3c59e16a3684b
Maxwell Henderson <mxwhenderson@gmail.com>
2024-05-20 17:25:50 -0700

    m920q: Give ollama a home directory
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/m920q/services.nix
+++ hosts/m920q/services.nix
@@ -1,8 +1,22 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.ollama = {
-      enable = true;
-      listenAddress="0.0.0.0:11434";
+  users.users.ollama = {
+    isSystemUser = true;
+    group = "ollama";
+    createHome = true;
+    home = "/srv/ollama";
   };
+
+  services.ollama = {
+    enable = true;
+    home = "/srv/ollama";
+    models = "/srv/ollama/models";
+    listenAddress="0.0.0.0:11434";
+    writablePaths = [
+      "/src/ollama"
+    ];
+  };
+
+  systemd.services.ollama.serviceConfig.User = "ollama";
 }

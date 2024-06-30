7c268cacbe916528a3611c547c0fca123e456007
Maxwell Henderson <mxwhenderson@gmail.com>
2024-06-30 17:20:33 -0400

    rpi4: Update where the vpn points to
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/rpi4/vpn.nix
+++ hosts/rpi4/vpn.nix
@@ -3,7 +3,7 @@
 {
   # enable NAT
   networking.nat.enable = true;
-  networking.nat.externalInterface = "eth0";
+  networking.nat.externalInterface = "end0";
   networking.nat.internalInterfaces = [ "wg0" ];
   networking.firewall = {
     allowedUDPPorts = [ 51820 ];
@@ -16,14 +16,25 @@
       listenPort = 51820;
 
       postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o end0 -j MASQUERADE
       '';
 
       postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o end0 -j MASQUERADE
       '';
 
       privateKeyFile = "${config.sops.secrets.wireguard-private-key.path}";
+
+      peers = [
+        {
+          publicKey = "K26Ii1PeoV/MiHnlR5AVliT1Gxcj6l9e7d2a/JhxPGE=";
+          allowedIPs = [ "10.100.0.2/32" ];
+        }
+        {
+          publicKey = "kVl6rZc50ByZJC1COkFMmYV6krRY1hMh3HhqFG+OQyg=";
+          allowedIPs = [ "10.100.0.3/32" ];
+        }
+      ];
     };
   };
 }

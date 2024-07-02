73243fb7287c21f15f718a25a702cf89b5ce175f
Maxwell Henderson <mxwhenderson@gmail.com>
2024-07-01 22:04:24 -0700

    rpi4: Attempt to fix vpn connection issue
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/rpi4/default.nix
+++ hosts/rpi4/default.nix
@@ -74,9 +74,9 @@
     allowedTCPPorts = [
       22
     ];
+    trustedInterfaces = [ "wg0" ];
     extraCommands = ''
       iptables -A INPUT -p tcp --dport 22 -s 192.168.1.0/24 -j ACCEPT
-      iptables -A INPUT -p tcp --dport 22 -s 10.100.0.0/24 -j ACCEPT
       iptables -A INPUT -p tcp --dport 22 -j DROP
     '';
   };

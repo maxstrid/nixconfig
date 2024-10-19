0969009526651f4e2d728919726b9d447c899ce6
Maxwell Henderson <mxwhenderson@gmail.com>
2024-10-18 20:58:31 -0700

    m920q: Remove the ollama service
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/m920q/services.nix
+++ hosts/m920q/services.nix
@@ -1,46 +1,4 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Ollama
-  users.groups.ollama = {};
-
-  users.users.ollama = {
-    isSystemUser = true;
-    group = "ollama";
-    createHome = true;
-    home = "/srv/ollama";
-  };
-
-  services.ollama = {
-    enable = true;
-    home = "/srv/ollama";
-    models = "/srv/ollama/models";
-    host = "0.0.0.0:11434";
-    writablePaths = [
-      "/srv/ollama"
-    ];
-  };
-
-  systemd.services.ollama.serviceConfig = {
-    User = "ollama";
-    Group = "ollama";
-  };
-
-  # llama.cpp
-  nixpkgs.overlays = [
-    (self: super: {
-      llama-cpp = super.llama-cpp.override {
-        rpcSupport = true;
-      };
-    })
-  ];
-
-  environment.systemPackages = [
-    pkgs.llama-cpp
-  ];
-
-  environment.sessionVariables = {
-    # This is to make llama-cpp find the libOpenCL.so.1 library
-    LD_LIBRARY_PATH="${pkgs.ocl-icd}/lib:$LD_LIBRARY_PATH";
-  };
 }

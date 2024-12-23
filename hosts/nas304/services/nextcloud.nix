afd1d31fdc68c4c8c3b99531ec2f7a48af1e935a
Maxwell Henderson <mxwhenderson@gmail.com>
2024-12-23 12:02:50 -0800

    nas304: Fix nextcloud config
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/nas304/services/nextcloud.nix
+++ hosts/nas304/services/nextcloud.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
     services.nextcloud = {
         enable = true;
         package = pkgs.nextcloud28;
         hostName = "nas304.localdomain/nextcloud";
-        config.adminpassFile = config.sops.secrets.nextcloud-adminpass.path
-    }
+        config.adminpassFile = config.sops.secrets.nextcloud-adminpass.path;
+    };
 }

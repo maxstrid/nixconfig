c89052b98f4ade6b04c78f07c4b1170babec833f
Maxwell Henderson <mxwhenderson@gmail.com>
2024-12-23 12:06:29 -0800

    nas304: Add nextcloud as owner to nextcloud secret
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/nas304/sops.nix
+++ hosts/nas304/sops.nix
@@ -5,6 +5,8 @@
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.secrets = {
       "wg0.conf" = {};
-      nextcloud-adminpass = {};
+      nextcloud-adminpass = {
+          owner = "nextcloud";
+      };
   };
 }

c3006110e3b66f83f17e34602a268afe5b791d01
Maxwell Henderson <mxwhenderson@gmail.com>
2024-12-23 12:55:14 -0800

    nas304: Por favor work
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/nas304/services/nextcloud.nix
+++ hosts/nas304/services/nextcloud.nix
@@ -4,9 +4,8 @@
     services.nextcloud = {
         enable = true;
         package = pkgs.nextcloud30;
-        hostName = "localhost";
+        hostName = "nextcloud.nas304.localdomain";
         config.adminpassFile = config.sops.secrets.nextcloud-adminpass.path;
         database.createLocally = true;
-        settings.trusted_proxies = ["127.0.0.1"];
     };
 }

5635ea27f5ab6f1b4eafbf37a1d7022124a40b5c
Maxwell Henderson <mxwhenderson@gmail.com>
2024-12-23 12:03:56 -0800

    nas304: Update nextcloud version
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- hosts/nas304/services/nextcloud.nix
+++ hosts/nas304/services/nextcloud.nix
@@ -3,7 +3,7 @@
 {
     services.nextcloud = {
         enable = true;
-        package = pkgs.nextcloud28;
+        package = pkgs.nextcloud30;
         hostName = "nas304.localdomain/nextcloud";
         config.adminpassFile = config.sops.secrets.nextcloud-adminpass.path;
     };
